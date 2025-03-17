1cfef3d5f1bdddfd5adda25b19423c206eff9e18
Kingson.Arum <nwajiudo@gmail.com>
2025-03-18 00:00:44 +0100

    write tests for token creation

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
+import {Token} from "./Token.sol";
 
 contract Factory {
-    uint256 public fee;
+    uint256 public immutable i_fee;
+    address public owner;
 
     constructor(uint256 _fee) {
-        fee = _fee;
+        i_fee = _fee;
+        owner = msg.sender;
+    }
+
+    function create() {
+        // create a new token
+        //save the token for later use
+        //list the token for sale
+        //tell people it's Live
     }
 }
--- test/Factory.js
+++ test/Factory.js
@@ -3,12 +3,28 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 describe("Factory", function () {
+    const FEE = ethers.parseUnits("0.01", 18);
+
 
     async function deployFactoryFixture() {
+        // Fetch accounts
+        const [deployer] = await ethers.getSigners();
         // Fetch the contract
         const Factory = await ethers.getContractFactory("Factory")
         // Deploy the contract
-        const factory = await Factory.deploy()
-        return { factory }
+        const factory = await Factory.deploy(FEE);
+        return { factory, deployer }
     }
+
+    describe("deployment", function () {
+        it("should set a fee", async function () {
+            const { factory } = await loadFixture(deployFactoryFixture)
+            expect(await factory.i_fee()).to.equal(FEE)
+        })
+
+        it("should set the Owner", async function () {
+            const { factory, deployer } = await loadFixture(deployFactoryFixture);
+            expect(await factory.owner()).to.equal(deployer.address);
+        })
+    })
 })
