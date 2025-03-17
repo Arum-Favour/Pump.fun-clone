c906355af0d6352d8a6ba88ff98b95d984a279b7
Kingson.Arum <nwajiudo@gmail.com>
2025-03-17 20:17:13 +0100

    Update Solidity version to 0.8.28 and enhance Factory and Token contracts with basic functionality

--- test/Factory.js
+++ test/Factory.js
@@ -4,4 +4,13 @@ const { ethers } = require("hardhat")
 
 describe("Factory", function () {
 
+    it("should have a name", async function () {
+        // Fetch the contract
+        const Factory = await ethers.getContractFactory("Factory")
+        // Deploy the contract
+        const factory = await Factory.deploy()
+        //check the name
+        const name = await factory.name()
+        console.log(name);
+    })
 })
